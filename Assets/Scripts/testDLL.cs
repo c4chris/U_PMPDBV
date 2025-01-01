ebd0b0574a49fcecc0d801a64b6586863b8c6b55
Christian Iseli <christian.iseli@epfl.ch>
2025-01-01 13:31:56 +0100

    Use new debug delegate to receive printf messages from DLL

--- Assets/Scripts/testDLL.cs
+++ Assets/Scripts/testDLL.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Runtime.InteropServices;
+using System;
 
 ///
 /// Need to test this : https://www.codeproject.com/Tips/318140/How-to-make-a-callback-to-Csharp-from-C-Cplusplus
@@ -8,17 +9,29 @@ using System.Runtime.InteropServices;
 public class testDLL : MonoBehaviour
 {
     [DllImport("PMPDBVDLL")]
+    public static extern void DebugCBSetup(CallbackDebugDelegate callback);
+    [DllImport("PMPDBVDLL")]
     public static extern short InitGlobals();
+
+    public delegate void CallbackDebugDelegate(IntPtr msg);
+    private static CallbackDebugDelegate delegateInstance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       Debug.Log("InitGlobals() returned " +  InitGlobals()); 
+        delegateInstance = DebugDelegate;
+        DebugCBSetup(delegateInstance);
+        Debug.Log("InitGlobals() returned " +  InitGlobals()); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public static void DebugDelegate(IntPtr c)
+    {
+        string s = Marshal.PtrToStringAnsi(c);
+        Debug.Log("From DLL: " + s);
     }
 }
