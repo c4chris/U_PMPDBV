bedf6d0fc4b1696e465c5fef5e3caeca31074070
Christian Iseli <christian.iseli@epfl.ch>
2025-01-03 00:09:18 +0100

    Build the mesh with spheres for each atom of the molecule

--- Assets/Scripts/PDBentry.cs
+++ Assets/Scripts/PDBentry.cs
@@ -3,23 +3,203 @@ using UnityEngine.Networking;
 using System;
 using System.Collections;
 using System.Runtime.InteropServices;
-using static UnityEngine.Rendering.DebugUI;
-using Unity.VisualScripting;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class PDBentry : MonoBehaviour
 {
     public struct Point3D
     {
-        double x, y, z;
+        public double x, y, z;
         public override readonly string ToString() => $"({x},{y},{z})";
     };
 
     public struct FPoint3D
     {
-        float x, y, z;
+        public float x, y, z;
         public override readonly string ToString() => $"({x},{y},{z})";
     };
+    public struct CubeSphereGenerator
+	{
+        public int gridSize;
+
+        public float radius;
+
+        private Mesh mesh;
+        private Vector3[] vertices;
+        private Vector3[] normals;
+        private Color32[] cubeUV;
+
+        public Mesh Generate (string name) {
+            mesh = new Mesh();
+            mesh.name = name;
+            CreateVertices();
+            CreateTriangles();
+            return mesh;
+        }
+
+        private void CreateVertices () {
+            int cornerVertices = 8;
+            int edgeVertices = (gridSize + gridSize + gridSize - 3) * 4;
+            int faceVertices = (
+                (gridSize - 1) * (gridSize - 1) +
+                (gridSize - 1) * (gridSize - 1) +
+                (gridSize - 1) * (gridSize - 1)) * 2;
+            vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+            normals = new Vector3[vertices.Length];
+            cubeUV = new Color32[vertices.Length];
+
+            int v = 0;
+            for (int y = 0; y <= gridSize; y++) {
+                for (int x = 0; x <= gridSize; x++) {
+                    SetVertex(v++, x, y, 0);
+                }
+                for (int z = 1; z <= gridSize; z++) {
+                    SetVertex(v++, gridSize, y, z);
+                }
+                for (int x = gridSize - 1; x >= 0; x--) {
+                    SetVertex(v++, x, y, gridSize);
+                }
+                for (int z = gridSize - 1; z > 0; z--) {
+                    SetVertex(v++, 0, y, z);
+                }
+            }
+            for (int z = 1; z < gridSize; z++) {
+                for (int x = 1; x < gridSize; x++) {
+                    SetVertex(v++, x, gridSize, z);
+                }
+            }
+            for (int z = 1; z < gridSize; z++) {
+                for (int x = 1; x < gridSize; x++) {
+                    SetVertex(v++, x, 0, z);
+                }
+            }
+
+            mesh.vertices = vertices;
+            mesh.normals = normals;
+            mesh.colors32 = cubeUV;
+        }
+
+        private void SetVertex (int i, int x, int y, int z) {
+            Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
+            float x2 = v.x * v.x;
+            float y2 = v.y * v.y;
+            float z2 = v.z * v.z;
+            Vector3 s;
+            s.x = v.x * Mathf.Sqrt(1f - y2 / 2f - z2 / 2f + y2 * z2 / 3f);
+            s.y = v.y * Mathf.Sqrt(1f - x2 / 2f - z2 / 2f + x2 * z2 / 3f);
+            s.z = v.z * Mathf.Sqrt(1f - x2 / 2f - y2 / 2f + x2 * y2 / 3f);
+            normals[i] = s;
+            vertices[i] = normals[i] * radius;
+            cubeUV[i] = new Color32((byte)x, (byte)y, (byte)z, 0);
+        }
+
+        private void CreateTriangles () {
+            int[] trianglesZ = new int[(gridSize * gridSize) * 12];
+            int[] trianglesX = new int[(gridSize * gridSize) * 12];
+            int[] trianglesY = new int[(gridSize * gridSize) * 12];
+            int ring = (gridSize + gridSize) * 2;
+            int tZ = 0, tX = 0, tY = 0, v = 0;
+
+            for (int y = 0; y < gridSize; y++, v++) {
+                for (int q = 0; q < gridSize; q++, v++) {
+                    tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+                }
+                for (int q = 0; q < gridSize; q++, v++) {
+                    tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+                }
+                for (int q = 0; q < gridSize; q++, v++) {
+                    tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+                }
+                for (int q = 0; q < gridSize - 1; q++, v++) {
+                    tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+                }
+                tX = SetQuad(trianglesX, tX, v, v - ring + 1, v + ring, v + 1);
+            }
+
+            tY = CreateTopFace(trianglesY, tY, ring);
+            tY = CreateBottomFace(trianglesY, tY, ring);
+
+            mesh.subMeshCount = 3;
+            mesh.SetTriangles(trianglesZ, 0);
+            mesh.SetTriangles(trianglesX, 1);
+            mesh.SetTriangles(trianglesY, 2);
+        }
+
+        private int CreateTopFace (int[] triangles, int t, int ring) {
+            int v = ring * gridSize;
+            for (int x = 0; x < gridSize - 1; x++, v++) {
+                t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
+            }
+            t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+
+            int vMin = ring * (gridSize + 1) - 1;
+            int vMid = vMin + 1;
+            int vMax = v + 2;
+
+            for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++) {
+                t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + gridSize - 1);
+                for (int x = 1; x < gridSize - 1; x++, vMid++) {
+                    t = SetQuad(
+                        triangles, t,
+                        vMid, vMid + 1, vMid + gridSize - 1, vMid + gridSize);
+                }
+                t = SetQuad(triangles, t, vMid, vMax, vMid + gridSize - 1, vMax + 1);
+            }
+
+            int vTop = vMin - 2;
+            t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
+            for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++) {
+                t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
+            }
+            t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+
+            return t;
+        }
+
+        private int CreateBottomFace (int[] triangles, int t, int ring) {
+            int v = 1;
+            int vMid = vertices.Length - (gridSize - 1) * (gridSize - 1);
+            t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+            for (int x = 1; x < gridSize - 1; x++, v++, vMid++) {
+                t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
+            }
+            t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+
+            int vMin = ring - 2;
+            vMid -= gridSize - 2;
+            int vMax = v + 2;
+
+            for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++) {
+                t = SetQuad(triangles, t, vMin, vMid + gridSize - 1, vMin + 1, vMid);
+                for (int x = 1; x < gridSize - 1; x++, vMid++) {
+                    t = SetQuad(
+                        triangles, t,
+                        vMid + gridSize - 1, vMid + gridSize, vMid, vMid + 1);
+                }
+                t = SetQuad(triangles, t, vMid + gridSize - 1, vMax + 1, vMid, vMax);
+            }
+
+            int vTop = vMin - 1;
+            t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+            for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++) {
+                t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
+            }
+            t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+
+            return t;
+        }
+
+        private static int SetQuad (int[] triangles, int i, int v00, int v10, int v01, int v11) {
+            triangles[i] = v00;
+            triangles[i + 1] = triangles[i + 4] = v01;
+            triangles[i + 2] = triangles[i + 3] = v10;
+            triangles[i + 5] = v11;
+            return i + 6;
+        }
+
+    }
 
     public delegate void GL_atmDelegate(FPoint3D A, short layer, ushort group, ushort atom, byte chain, long color);
     public delegate void GL_bndDelegate(FPoint3D A, FPoint3D B, short layer, ushort group, ushort atomA, ushort atomB, byte chain, long color);
@@ -55,8 +235,10 @@ public class PDBentry : MonoBehaviour
     public delegate void CallbackDebugDelegate(IntPtr msg);
     private static CallbackDebugDelegate debugDelegateInstance;
     private static SwiftCallbacks callbacksInstance;
+    private static List<CombineInstance> meshList;
 
     public string entry = "1crn";
+    private int nb = 0;
     void Start()
     {
         debugDelegateInstance = DebugDelegate;
@@ -73,6 +255,7 @@ public class PDBentry : MonoBehaviour
             GL_Draw_Shape = GL_Draw_Shape
         };
         SwiftCBSetup(callbacksInstance);
+        meshList = new List<CombineInstance>();
         Debug.Log("InitGlobals() returned " +  InitGlobals()); 
         // Get entry
         StartCoroutine(GetText(entry, (path) => { EntryLoaded(path); }));
@@ -80,6 +263,17 @@ public class PDBentry : MonoBehaviour
 
     void Update()
     {
+        if (meshList.Count !=  nb)
+        {
+            nb = meshList.Count;
+            Mesh mesh = new Mesh();
+            // We can have more than 65k vertices per mesh...
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            mesh.CombineMeshes(meshList.ToArray());
+            // if we do not combine the submeshes, we have to set 1 material per submesh
+            //mesh.CombineMeshes(meshList.ToArray(), false);
+            GetComponent<MeshFilter>().sharedMesh = mesh;
+        }
     }
 
     private void EntryLoaded(string path)
@@ -87,6 +281,7 @@ public class PDBentry : MonoBehaviour
         Debug.Log("We have received entry " + entry + " at " + path);
         doPDBinput(path, entry);
         Debug.Log("We have loaded entry " + entry);
+        meshList.Clear();
         GLRender();
     }
 
@@ -125,6 +320,20 @@ public class PDBentry : MonoBehaviour
     {
         char c = Convert.ToChar(chain);
         Debug.Log("GL_atm(" + A + ", " + layer + ", " + group + ", " + atom + ", " + c + ", " + color + ")");
+        CubeSphereGenerator gen = new CubeSphereGenerator
+        {
+            radius = 1f,
+            gridSize = 5
+        };
+        CombineInstance ci = new CombineInstance
+        {
+            mesh = gen.Generate("name"),
+            transform = new Matrix4x4(new Vector4(1, 0, 0, 0),
+                                      new Vector4(0, 1, 0, 0),
+                                      new Vector4(0, 0, 1, 0),
+                                      new Vector4(A.x, A.y, A.z, 1))
+        };
+        meshList.Add(ci);
     }
     public static void GL_bnd(FPoint3D A, FPoint3D B, short layer, ushort group, ushort atomA, ushort atomB, byte chain, long color)
     {
